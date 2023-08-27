6357308b591c7be138a2f35cde9bc84032a4b8b5
HKH-Dev <1kevin@hotmail.es>
2023-08-27 11:11:53 -0500

    first info update of project

--- co.edu.uniquindio.student/src/main/java/co/edu/uniquindio/student/Main.java
+++ co.edu.uniquindio.student/src/main/java/co/edu/uniquindio/student/Main.java
@@ -1,7 +1,16 @@
 package co.edu.uniquindio.student;
 
+import co.edu.uniquindio.student.model.Student;
+
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student student001 = new Student("Juan David Romer Alvarado", 23, "jdra@gmail.com", "older", 4.3f, 2.1f, 4.5f);
+        Student student002 = new Student("Maria Perez", 21, "mperez@gmail.com", "younger", 3.5f, 2.8f, 4.1f);
+        Student student003 = new Student("Carlos Rodriguez", 22, "crodriguez@gmail.com", "younger", 4.0f, 3.2f, 4.5f);
+        Student student004 = new Student("Sofia Gomez", 20, "sgomez@gmail.com", "younger", 3.7f, 2.1f, 4.0f);
+        Student student005 = new Student("Andres Torres", 23, "atorres@gmail.com", "older", 4.5f, 3.9f, 4.7f);
+        
     }
 }
--- co.edu.uniquindio.student/src/main/java/co/edu/uniquindio/student/model/Student.java
+++ co.edu.uniquindio.student/src/main/java/co/edu/uniquindio/student/model/Student.java
@@ -1,4 +1,87 @@
 package co.edu.uniquindio.student.model;
 
 public class Student {
+    private String name;
+    private int age;
+    private String email;
+    private String kindOfStudent;
+    private float GradeOne;
+    private float GradeTwo;
+    private float GradeThree;
+
+    float[] grades = new float[3];
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKindOfStudent() {
+        return kindOfStudent;
+    }
+
+    public void setKindOfStudent(String kindOfStudent) {
+        this.kindOfStudent = kindOfStudent;
+    }
+
+    public float getGradeOne() {
+        return GradeOne;
+    }
+
+    public void setGradeOne(float gradeOne) {
+        GradeOne = gradeOne;
+    }
+
+    public float getGradeTwo() {
+        return GradeTwo;
+    }
+
+    public void setGradeTwo(float gradeTwo) {
+        GradeTwo = gradeTwo;
+    }
+
+    public float getGradeThree() {
+        return GradeThree;
+    }
+
+    public void setGradeThree(float gradeThree) {
+        GradeThree = gradeThree;
+    }
+
+    public float[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(float[] grades) {
+        this.grades = grades;
+    }
+
+    public Student(String name, int age, String email, String kindOfStudent, float gradeOne, float gradeTwo, float gradeThree) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.kindOfStudent = kindOfStudent;
+        GradeOne = gradeOne;
+        GradeTwo = gradeTwo;
+        GradeThree = gradeThree;
+    }
 }
